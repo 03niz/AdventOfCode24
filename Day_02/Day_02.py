6c52216aa45fb696f25ed56ac6718a90c96b05c3
03niz <nizarcarjoun@gmail.com>
2025-01-05 21:49:23 +0800

    Added Day 03

--- Day_02/Day_02.py
+++ Day_02/Day_02.py
@@ -6,11 +6,14 @@ def part1():
 
     # Opens and reads each line
     with open('input.txt', 'r') as file:
+        unsafe = 0
        
         for line in file:
 
             sequence = []
             diffSequence = []
+            harmony = ""
+
 
             sequence = line.split()
 
@@ -18,10 +21,35 @@ def part1():
                 diff = 0
                 diff = (int((sequence[i])) - int((sequence[i - 1])))
                 diffSequence.append(diff)
-            print(diffSequence)
+            
+            if int(diffSequence[0]) < 0:
+                polaritySet = "negaitve"
+            
+            if int(diffSequence[0]) >= 0:
+                polaritySet = "positve"
+
+            for i in range(len(diffSequence)):
+
+                polarity = ""
+                polaritySet = ""
                 
+                if int(diffSequence[i]) < 0:
+                    polaritySet = "negaitve"
+            
+                if int(diffSequence[i]) > 0:
+                    polaritySet = "positve"
+                
+                if polarity != polaritySet:
+                    harmony = "broken"
+            
+            if harmony == "broken":
+                unsafe = unsafe + 1
+            print(diffSequence)
 
-def main():
-    part1()
+    
+    print(unsafe)
 
-main()
+
+
+
+part1()
